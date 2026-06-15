74cf6801887c4b3c8856c80142a5f5f8dca1ae01
Zachary Martin <zackashm@gmail.com>
2026-06-15 14:18:38 -0500

    update nuclei doc for iron vs others disclaimer

--- cosmiclimits/nuclei/__init__.py
+++ cosmiclimits/nuclei/__init__.py
@@ -1,2 +1,6 @@
-"""Nuclear cosmic-ray interaction processes."""
+"""
+Nuclear cosmic-ray interaction processes.
+This module is written with Iron in mind, and has comments mentioning Iron specifically. 
+But the module could be used with other high mass nuclei as well with minimal changes.
+"""
 
